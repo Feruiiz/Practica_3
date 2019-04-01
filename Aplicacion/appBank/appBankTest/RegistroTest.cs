49b30fe478772cba81fa8d52a468f8a4b79694b4
JeffPC\Jefferson <karlosceron15@gmail.com>
2019-04-01 14:07:46 -0600

    Prueba Unitaria - Modulo de Registro - 201504448

---
+++ Aplicacion/appBank/appBankTest/RegistroTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using appBank;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace appBankTest
+{
+    [TestClass]
+    public class RegistroTest
+    {
+        [TestMethod]
+        public void TestCrearUsuario()
+        {
+            //PRUEBA PARA CREAR USUARIOS
+            String name = "Jefferson Linares";//nombre del usuario
+            String nick = "jeff1234";  //nickname debe contener un digito
+            String pass = "abcde123";  //contraseña de 8 digitos
+            String email = "jefflinares@gmail.com"; //correo electronico
+
+            Conexion c = new Conexion();
+
+            Boolean respuesta = c.crearUsuario(name, nick, pass, email);
+
+            Assert.IsTrue(respuesta); //si la respuesta es verdadera el metodo esta bien y si crea al usuario
+        }
+    }
+}
