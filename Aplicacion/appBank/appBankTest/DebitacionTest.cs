dedbcdadaf71e51f9b2628abf6135e59723e01c4
FeRuiz <feruiz.12000@gmail.com>
2019-04-13 14:51:25 -0600

    PruebaUnitaria_Debitaciones

---
+++ Aplicacion/appBank/appBankTest/DebitacionTest.cs
@@ -0,0 +1,26 @@
+﻿using appBank;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace appBankTest
+{
+    [TestClass]
+    public class DebitacionTest
+    {
+        Conexion c = new Conexion();
+        [TestMethod]
+        public void Debitacion()
+        {
+            double saldoActual = 2000;
+            double saldoActual2 = 300;
+            double monto = 200;
+            int cuenta = 1;
+            int cuenta2 = 2;
+            Double newSaldo = Convert.ToDouble(saldoActual) - Convert.ToDouble(monto);
+            Boolean resp1 = c.Ejecutar201503984("UPDATE Cuenta SET saldo = " + newSaldo.ToString() + " WHERE idCuenta = " + cuenta.ToString() + ";");
+            newSaldo = Convert.ToDouble(saldoActual2) - Convert.ToDouble(monto.ToString());
+            Boolean resp2 = c.Ejecutar201503984("UPDATE Solicitud_Prestamo SET monto = " + newSaldo.ToString() + " WHERE idSolicitud = " + cuenta2.ToString() + ";");
+            Assert.IsNotNull(resp1 && resp2);
+        }
+    }
+}
