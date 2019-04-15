efc920e499cd87852de32bf0e54f08028ee44a8d
JeffPC\Jefferson <karlosceron15@gmail.com>
2019-04-15 14:57:48 -0600

    Test Reporte Notificaciones arreglado - 201504448

--- Aplicacion/appBank/appBankTest/reporteNotificacionesTest.cs
+++ Aplicacion/appBank/appBankTest/reporteNotificacionesTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using appBank;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace appBankTest
@@ -8,10 +9,10 @@ namespace appBankTest
     {
         Conexion c = new Conexion();
         [TestMethod]
-        public void TestMethod1()
+        public void NotificacionesTest()
         {
             // CONSULTA PARA VER LAS NOTIFICACIONES
-            String query = "SELECT n.mensaje, n.fecha, u.nombre, n.estado FROM Notificacion n, Cuenta c, Usuario u WHERE n.cuentaReceptora = " + Session["idCuenta"].ToString() + " and n.cuentaEmisora = c.idCuenta and c.idUsuario = u.idUsuario ORDER BY n.idNotificacion DESC; ";
+            String query = "SELECT n.mensaje, n.fecha, u.nombre, n.estado FROM Notificacion n, Cuenta c, Usuario u WHERE n.cuentaReceptora = 1 and n.cuentaEmisora = c.idCuenta and c.idUsuario = u.idUsuario ORDER BY n.idNotificacion DESC; ";
             bool resp1 = c.Ejecutar201503984(query);
             Assert.IsTrue(resp1);
         }
