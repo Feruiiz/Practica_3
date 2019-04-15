38ad1b99ecb2413dc2acbef57d9f886554769b40
JeffPC\Jefferson <karlosceron15@gmail.com>
2019-04-15 14:42:36 -0600

    Test Reporte Notificaciones - 201504448

---
+++ Aplicacion/appBank/appBankTest/reporteNotificacionesTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace appBankTest
+{
+    [TestClass]
+    public class reporteNotificacionesTest
+    {
+        Conexion c = new Conexion();
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // CONSULTA PARA VER LAS NOTIFICACIONES
+            String query = "SELECT n.mensaje, n.fecha, u.nombre, n.estado FROM Notificacion n, Cuenta c, Usuario u WHERE n.cuentaReceptora = " + Session["idCuenta"].ToString() + " and n.cuentaEmisora = c.idCuenta and c.idUsuario = u.idUsuario ORDER BY n.idNotificacion DESC; ";
+            bool resp1 = c.Ejecutar201503984(query);
+            Assert.IsTrue(resp1);
+        }
+    }
+}
