d535c25e72df07dee24fc6341ef03afdd8450f02
JeffPC\Jefferson <karlosceron15@gmail.com>
2019-04-15 14:43:56 -0600

    Test Reporte Solicitudes - 201504448

---
+++ Aplicacion/appBank/appBankTest/reporteSolicitudesTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using appBank;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace appBankTest
+{
+    [TestClass]
+    public class reporteSolicitudesTest
+    {
+        Conexion c = new Conexion();
+        [TestMethod]
+        public void solicitudesTest()
+        {
+            //CONSULTA PARA OBTENER LAS SOLICITUDES
+            String query = "SELECT c.idCuenta, u.nombre, c.saldo, s.monto, s.descripcion, s.estado FROM Solicitud_Prestamo s, Cuenta c, Usuario u WHERE s.idCuenta = c.idCuenta and c.idUsuario = u.idUsuario;";
+            bool resp1 = c.Ejecutar201503984(query);
+            Assert.IsTrue(resp1);
+
+        }
+    }
+}
